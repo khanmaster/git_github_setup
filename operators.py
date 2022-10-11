f545fd1e1ee52a0dac110d96ac9cef16bf4f1987
Abdul Haseeb Shahrukh Khan <ahskhan@spartaglobal.com>
2022-10-11 11:26:02 +0100

    operators lesson with example code added

---
+++ operators.py
@@ -0,0 +1,23 @@
+# Let's see the operators in actoin
+### Intro to Data types & Operators
+# - `+ - * /`
+#
+# ###### Comparison Operators
+# - `>` greater than
+# - `<` less than
+# - `==` True or False
+# - `>=` greater than or equal
+# - `<=` less than or equal
+
+a = 24
+b = 16
+user_age = 18
+print(a + b) # outcome added  value of a & b
+print(a - b) # outcome - a from b
+# comparison
+print(a > b) # True
+print(a < b) # False
+print(a == b)
+
+# % operator - find out what it is how it's used in python
+# create a print statement to show the use case of it
